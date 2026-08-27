2669088ca535343fb608559f295984d736a705fb
My DSA journey <antordey2005@gmail.com>
2026-08-27 12:50:45 +0600

    new code

--- My coding/basic_code/tempCodeRunnerFile.cpp
+++ My coding/basic_code/tempCodeRunnerFile.cpp
@@ -1,4 +1,17 @@
 #include<iostream>
 using namespace std;
-
 int main(){
+    int temp;
+    int a,b;
+    
+cout<<"Enter a & b: ";
+cin>>a;
+cin>>b;
+
+temp=a;
+a=b;
+b=temp;
+
+cout<<a<<" "<<b;
+
+}
