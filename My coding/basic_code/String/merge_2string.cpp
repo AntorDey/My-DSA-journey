89d70ce3f8f988e0f9f0330bcaf9f61c3a11b736
My DSA journey <antordey2005@gmail.com>
2026-09-15 12:17:20 +0600

    marge

---
+++ My coding/basic_code/String/merge_2string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s1 = "Antor";
+    string s2 = "Dey";
+    string s3 = s1+s2;
+    cout<<s3<<endl;
+    cout<<"size is:"<<s3.size();
+   return 0 ; 
+}
