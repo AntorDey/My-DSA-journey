2686bf558806361c068f21ced456691657d198f5
My DSA journey <antordey2005@gmail.com>
2026-09-15 11:54:30 +0600

    string

---
+++ My coding/basic_code/String/intro_code.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char arr[] = {'A','P','P','L','E'};
+//     //for(int i = 0 ; i<5; i++)
+
+//        //arr[2]= '\0';
+//     cout<<arr[i];
+
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+//     string s = "Antor DEY";     //string print ;
+//     cout<<s;
+// }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s;           // string input;
+    cin>>s;
+    cout << s;
+}
