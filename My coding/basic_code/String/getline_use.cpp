fc8b580b4cfcfb72a0e6c7eb2110df80405df856
My DSA journey <antordey2005@gmail.com>
2026-09-15 12:12:10 +0600

    getline and finding size

---
+++ My coding/basic_code/String/getline_use.cpp
@@ -0,0 +1,17 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     string s;
+//     getline(cin,s);  //for print next space part 
+//     cout<<s;
+// }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s= "Antor";  //finding size 
+    
+    cout<<s.size();
+}
