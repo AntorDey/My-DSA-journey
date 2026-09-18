4becb4d07b8be988f686c711ad75f69323fa2b9d
My DSA journey <antordey2005@gmail.com>
2026-09-19 01:06:23 +0600

    small update

--- My coding/basic_code/String/PelindromeCheck.cpp
+++ My coding/basic_code/String/PelindromeCheck.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string s = "abcbaq";
+    string s = "aabbcbbaa.";
     int start = 0, end = s.size()-1;
     while(start<end){
         if(s[start] != s[end]){
             cout<<"Not a pelindrome";
             return 0;
         }
+        if(s[start]==s[end])
         start ++;
         end --;
 
